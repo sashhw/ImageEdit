224cb6c8095c8d20daa256b3229a93d517e7d0dc
sashhw <sash.walk@gmail.com>
2021-07-06 10:21:41 -0500

    Initial Commit

---
+++ ImageEdit/ImageEditApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageEditApp.swift
+//  ImageEdit
+//
+//  Created by Sasha on 7/6/21.
+//
+
+import SwiftUI
+
+@main
+struct ImageEditApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
